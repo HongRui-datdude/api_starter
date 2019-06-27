5b25d6d37aee41b5e2c3a372890bf338251eb6ed
LC4311 <16984757+LC4311@users.noreply.github.com>
2019-06-27 16:17:46 +0800

    added credentials and security

---
+++ security.js
@@ -0,0 +1,13 @@
+import {apikey} from 'credentials'
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: apikey,
+    authDomain: "test-2c2ca.firebaseapp.com",
+    databaseURL: "https://test-2c2ca.firebaseio.com",
+    projectId: "test-2c2ca",
+    storageBucket: "test-2c2ca.appspot.com",
+    messagingSenderId: "90534988206",
+    appId: "1:90534988206:web:edd8a589130a3306"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
